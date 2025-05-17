e13f514b13a2b5647fe8ba4aaa81599234531b75
GraemeBadley <17gb12@queensu.ca>
2025-05-17 16:58:56 -0400

    added the locationas web scraping

---
+++ web_scraper/locations.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+def subcategories(links):
+
+    sub_location_names = []
+
+    for link in links:
+        response = requests.get('https://stardewvalleywiki.com' + link.get('href'))
+        sub_html = response.text
+        sub_soup = BeautifulSoup(sub_html,'html.parser')
+
+        sub_location_links = sub_soup.select("#mw-pages a")
+
+        sub_location_names += list(map(lambda link: link.text, sub_location_links))
+
+    return sub_location_names
+
+def get_locations():
+    base_url = 'https://stardewvalleywiki.com/Category:Locations'
+
+    response = requests.get(base_url)
+
+    html = response.text
+
+
+
+    soup = BeautifulSoup(html,'html.parser')
+
+    #print(soup.select('#mw-subcategories a')[0].get('href'))
+
+    location_names = soup.select('#mw-pages a')
+
+    location_names = list(map(lambda link: link.text, location_names))
+
+
+
+
+    location_names += subcategories(soup.select('#mw-subcategories a'))
+
+    return list(set(location_names))
+
+def insert_locations(cur):
+
+    locations_names = get_locations()
+
+    sql_string = "INSERT INTO locations(location_name) VALUES (%s);" 
+    
+    cur.executemany(sql_string, [(name,) for name in locations_names])
+    print(cur.rowcount)
---
+++ web_scraper/main.py
@@ -0,0 +1,25 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+import locations
+
+env_path = "../.env.development.local"
+
+load_dotenv(dotenv_path=env_path)
+os.getenv("DATABASE_URL")
+conn = psycopg2.connect(
+    dbname= os.getenv("PGDATABASE"),
+    user= os.getenv("PGUSER"),
+    password= os.getenv("POSTGRES_PASSWORD"),
+    host= os.getenv("POSTGRES_HOST"),
+    port= 5432,
+    sslmode="require"
+)
+
+cur = conn.cursor()
+
+locations.insert_locations(cur)
+
+conn.commit()
+cur.close()
+conn.close()
